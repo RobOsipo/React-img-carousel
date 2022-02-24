9f4e8d20fa4461edb02ff898780ceda73e64411f
Robert Osipovitch <90695804+RobOsipo@users.noreply.github.com>
2022-02-24 12:54:44 -0500

    simple React setup toggle dark/light

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+export default function App() {
+    const [darkMode, setDarkMode] = React.useState(false)
+    function toggle() {
+        setDarkMode(prevState => !prevState)
+    }
+    return (
+        <div className="container">
+            <Navbar
+            toggleDarkMode={toggle}
+             darkMode={darkMode} 
+             />
+            <Main 
+            
+            darkMode={darkMode} 
+            />
+        </div>
+    )
+}
